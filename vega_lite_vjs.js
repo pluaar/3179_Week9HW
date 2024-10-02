65d3669050daff4d4c6283f14d6286f847c068ad
pluaar <127108470+pluaar@users.noreply.github.com>
2024-10-02 23:00:48 +0800

    Create vega_lite_vjs.js

---
+++ vega_lite_vjs.js
@@ -0,0 +1,4 @@
+var vg1 = "map_vis.json";
+vegaEmbed("#map_vis", vg1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
